5860b0abf3269e676a7ba6ff857a0b4aaf6f87a7
Latte <bjwq2019@gmail.com>
2024-05-08 14:13:18 -0400

    v4

--- user/find.c
+++ user/find.c
@@ -51,10 +51,11 @@ void search(char *path, char *searchName) {
     switch(st.type){
   case T_FILE:
     if(strcmp(fmtname(path), searchName) == 0) {
-    printf("%s\n", path);
+        printf("%s\n", path);
     }
-    // printf("%s\n", fmtname(path));
-    // printf("%s\n", searchName);
+    printf("%s\n", fmtname(path));
+    printf("%s\n", searchName);
+    printf("%d\n", strcmp(fmtname(path), searchName));
     break;
 
   case T_DIR:
