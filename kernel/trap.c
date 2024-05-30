50a76e27025fbb9c4c102bf93237953ede7eafbb
Latte <bjwq2019@gmail.com>
2024-05-30 12:48:26 -0400

    test0

--- kernel/trap.c
+++ kernel/trap.c
@@ -77,13 +77,15 @@ usertrap(void)
     exit(-1);
 
   // give up the CPU if this is a timer interrupt.
-  if(which_dev == 2)
+  if(which_dev == 2) {
     p->ticks++;
     if (p->alarminterval > 0 && p->ticks % p->alarminterval == 0) {
       p->ticks = 0;
       p->trapframe->epc = p->handler;
     }
     yield();
+  }
+    
 
   usertrapret();
 }
