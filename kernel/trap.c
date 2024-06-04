c49aa516e75dc8c499ef92bf27486b27089f2b30
Latte <bjwq2019@gmail.com>
2024-06-04 00:09:36 -0400

    debug

--- kernel/trap.c
+++ kernel/trap.c
@@ -72,9 +72,8 @@ usertrap(void)
     if(r_scause() == 13 || r_scause() == 15) {
       if (uvmshouldtouch(va)) {
         uvmtouch(va);
-        return;
       }
-      kill(p->pid);
+
     } else {
       printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
       printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
