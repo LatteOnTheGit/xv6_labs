b31fe44259e521dc1c59ec3433cc3144e65b750d
Latte <bjwq2019@gmail.com>
2024-07-02 21:16:29 -0400

    second attempt large files

--- kernel/fs.c
+++ kernel/fs.c
@@ -416,7 +416,7 @@ bmap(struct inode *ip, uint bn)
     bp2 = bread(ip->dev, addr);
     a2 = (uint*)bp2->data;
     if ((addr = a2[bn % NINDIRECT]) == 0) {
-      a[bn % NINDIRECT] = addr = balloc(ip->dev);
+      a2[bn % NINDIRECT] = addr = balloc(ip->dev);
       log_write(bp2);
     }
     brelse(bp);
